9de73b3981d433067aa7b3e3c4403e4084ac5859
cta014 <cta014@uit.no>
2023-02-03 10:54:47 +0000

    updated provision file

---
+++ S1_sea_ice_classification/provision.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Use this file to provision new conda python environment
+# with packages for CVL environment
+
+conda install -c conda-forge \
+  gdal \
+  netcdf4 \
+  geopandas \
+  xarray \
+  dask \
+  cartopy \
+  loguru \
+  python-dotenv \
+  sentinelsat \
+  scikit-learn \
