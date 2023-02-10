c844e306660c70e77f1d44fbe8dd128d463cc9be
Yannick <yannick.kern@npolar.no>
2023-02-10 22:53:15 +0000

    restore uwibasspp

---
+++ netCDF-creator/uwibasspp.py
@@ -0,0 +1,30 @@
+# UWiBaSS object init
+
+
+
+class rdr:
+    def __init__(self):
+        # self.timestamp = None
+        # self.slowtime = None
+        # self.rx = None
+        # self.rx1 = None
+        # self.rx2 = None
+        # self.fasttime = None
+        # self.rxsvd = None
+        # self.range_air = None
+        self.rxc = None
+
+        ## Stuff coming in later
+        # self.lat = None
+        # self.lon = None
+        # self.alt = None
+        # self.roll = None
+        # self.pitch = None
+        # self.yaw = None
+        # self.rngf = None
+        # self.dist = None
+        # self.top = None
+        # self.bottom = None
+        # uwibass.bottom_no_rec = None
+
+
